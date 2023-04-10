c109b920b3b8ea6cda9aee30e2ca6b5dc5679ad3
Nego <61694826+kardespro@users.noreply.github.com>
2023-04-10 10:12:03 +0000

    New Update !

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+       <title>Next-Binfo - Lookup Next.js Websites</title>
+        <link rel="icon" href="/external/logox.png" />
+      </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
