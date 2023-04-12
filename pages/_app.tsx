0af104a11659ac5e18a20601953d8645fe80e332
kardespro <61694826+kardespro@users.noreply.github.com>
2023-04-12 20:35:33 +0000

    Vercel Analytics Support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/external/logox.png" />
       </Head>
     <Component {...pageProps} />
+    <Analytics />
     </>
   )
 }
